2210633fa86ab895bffad076916d82c4e2c585a4
Zueva Elena <laverdad2@yandex.ru>
2025-06-23 12:38:47 +0300

    feature/di-java

---
+++ 04_serlvets/servlets/src/main/java/ru/netology/config/AppConfig.java
@@ -0,0 +1,17 @@
+@Configuration
+public class AppConfig {
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepository();
+    }
+
+    @Bean
+    public PostService postService() {
+        return new PostService(postRepository());
+    }
+
+    @Bean
+    public PostController postController() {
+        return new PostController(postService());
+    }
+}
